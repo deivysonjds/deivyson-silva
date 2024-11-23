c82b86bb6e63d54df1fde7061e2ece4b1a8c3278
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-11-23 15:01:47 +0000

    modo light ajustado

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     }, {
-      threshold: 0.5  
+      threshold: 0.05
     });
   
     
@@ -26,4 +26,38 @@ whatsapp.onclick = () =>{
   let url = `https://api.whatsapp.com/send?phone=5581988993441&text=${encodeURIComponent(text)}`
   window.open(url, "_blank")
 }
+
+const lightButtonMode = document.getElementById("icon-mode");
+
+
+lightButtonMode.onclick = () => {
+  const body = document.body;
+  const textWhit = document.querySelectorAll('div#experiencias p, div#experiencias h3, div#hab h2, div#formacao p, div#formacao h3, div#formacao button')
+  const imgPerfil = document.getElementById('img-header')
+
+  body.classList.toggle("light");
+  
+  if (body.classList.contains("light")) {
+    localStorage.setItem("light", "enabled");
+  } else {
+    localStorage.setItem("light", "disabled");
+  }
+  
+  if (localStorage.getItem("light") === "enabled") {
+    
+    lightButtonMode.src = "./svg/icons8-lua-crescente-50.png"
+    body.classList.add("light");
+    imgPerfil.classList.add('light')
+    textWhit.forEach((e)=>{
+      e.classList.add("light")
+    })
+  } else {
+    lightButtonMode.src = "./svg/icons8-sun.svg"
+    body.classList.remove("light");
+    imgPerfil.classList.remove('light')
+    textWhit.forEach((e)=>{
+      e.classList.remove("light")
+    })
+  }
+};
   
