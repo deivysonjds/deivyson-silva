7ca82e9007a00fee3e9094f3524c65d20460f2a9
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-11-23 20:39:09 +0000

    atualizado modo claro

--- script.js
+++ script.js
@@ -32,9 +32,10 @@ const lightButtonMode = document.getElementById("icon-mode");
 
 lightButtonMode.onclick = () => {
   const body = document.body;
-  const textWhit = document.querySelectorAll('div#experiencias p, div#experiencias h3, div#hab h2, div#formacao p, div#formacao h3, div#formacao button, header p, header h1, div#container-sobre h2, div#container-sobre p, div#projetos h3, p, button')
+  const textWhit = document.querySelectorAll('div#experiencias p, div#experiencias h3, div#hab h2, div#formacao p, div#formacao h3, div#formacao button, header p, header h1, div#container-sobre h2, div#container-sobre p, div#projetos h3,div#projetos p,div#projetos button, div#container-exp h2, div#container-form h2')
   const imgPerfil = document.getElementById('img-header')
   const redes = document.querySelectorAll('div#redes svg g')
+  const divInfos = document.querySelectorAll('div#sobre, div#formacao .item, div#experiencias .item, div#projetos .item')
   body.classList.toggle("light");
   
   if (body.classList.contains("light")) {
@@ -46,6 +47,7 @@ lightButtonMode.onclick = () => {
   if (localStorage.getItem("light") === "enabled") {
     
     lightButtonMode.src = "./svg/icons8-lua-crescente-50.png"
+    lightButtonMode.classList.add('light')
     body.classList.add("light");
     imgPerfil.classList.add('light')
     textWhit.forEach((e)=>{
@@ -55,8 +57,13 @@ lightButtonMode.onclick = () => {
     redes.forEach((e)=>{
       e.classList.add("light")
     })
+
+    divInfos.forEach((e)=>{
+      e.classList.add("light")
+    })
   } else {
     lightButtonMode.src = "./svg/icons8-sun.svg"
+    lightButtonMode.classList.remove('light')
     body.classList.remove("light");
     imgPerfil.classList.remove('light')
     textWhit.forEach((e)=>{
@@ -64,6 +71,10 @@ lightButtonMode.onclick = () => {
     })
 
     redes.forEach((e)=>{
+      e.classList.remove("light")
+    })
+
+    divInfos.forEach((e)=>{
       e.classList.remove("light")
     })
   }
