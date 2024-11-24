a7d05916a5a3704950461ef0d989aabb3f7ba1f0
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-11-24 03:38:25 +0000

    atualizado modo dark com salvamento de preferências

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
+const body = document.body;
+const textWhit = document.querySelectorAll('div#experiencias p, div#experiencias h3, div#hab h2, div#formacao p, div#formacao h3, div#formacao button, header p, header h1, div#container-sobre h2, div#container-sobre p, div#projetos h3,div#projetos p,div#projetos button, div#container-exp h2, div#container-form h2, div#habilidades .item p')
+const imgPerfil = document.getElementById('img-header')
+const redes = document.querySelectorAll('div#redes svg g')
+const divInfos = document.querySelectorAll('div#sobre, div#formacao .item, div#experiencias .item, div#projetos .item, div#habilidades .item')
+
 document.addEventListener('DOMContentLoaded', () => {
-    const elements = document.querySelectorAll('.obs');
-  
-    
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
-          observer.unobserve(entry.target); 
-        }
-      });
-    }, {
-      threshold: 0.05
+  const elements = document.querySelectorAll('.obs');
+
+  if (localStorage.getItem("light") === "enabled") {
+
+    lightButtonMode.src = "./svg/icons8-lua-crescente-50.png"
+    lightButtonMode.classList.add('light')
+    body.classList.add("light");
+    imgPerfil.classList.add('light')
+    textWhit.forEach((e)=>{
+      e.classList.add("light")
+    })
+
+    redes.forEach((e)=>{
+      e.classList.add("light")
+    })
+
+    divInfos.forEach((e)=>{
+      e.classList.add("light")
+    })
+  }
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target); 
+      }
     });
-  
-    
-    elements.forEach(element => {
-      observer.observe(element);
-    });
+  }, {
+    threshold: 0.05
   });
+  
+    
+  elements.forEach(element => {
+    observer.observe(element);
+  });
+});
 
 const whatsapp = document.getElementById('whatsapp')
 
@@ -29,13 +53,8 @@ whatsapp.onclick = () =>{
 
 const lightButtonMode = document.getElementById("icon-mode");
 
-
 lightButtonMode.onclick = () => {
-  const body = document.body;
-  const textWhit = document.querySelectorAll('div#experiencias p, div#experiencias h3, div#hab h2, div#formacao p, div#formacao h3, div#formacao button, header p, header h1, div#container-sobre h2, div#container-sobre p, div#projetos h3,div#projetos p,div#projetos button, div#container-exp h2, div#container-form h2, div#habilidades .item p')
-  const imgPerfil = document.getElementById('img-header')
-  const redes = document.querySelectorAll('div#redes svg g')
-  const divInfos = document.querySelectorAll('div#sobre, div#formacao .item, div#experiencias .item, div#projetos .item, div#habilidades .item')
+  
   body.classList.toggle("light");
   
   if (body.classList.contains("light")) {
